74955a07fee73ff323088483868fecbdcdf118c7
Sarthakischauhan <sarthakchauhann@gmail.com>
2024-10-12 15:27:55 -0700

    This should change all font issues

--- app/components/ExternalCTA.jsx
+++ app/components/ExternalCTA.jsx
@@ -4,9 +4,8 @@ const ExternalCTA = ({action, content, icon}) => {
       <Link
         href={action}
         rel="noopener noreferrer"
-        className="group inline-flex items-center space-x-1 text-[14px] font-medium text-gray-700 hover:text-gray-900 text-gray-300 hover:text-gray-100 bg-[#202020] hover:bg-[#282828] border border-[rgba(255,255,255,0.1)] rounded px-2.5 py-1.5 transition-colors duration-150"
+        className="group inline-flex items-center space-x-1 text-[14px] font-medium text-gray-300 hover:text-gray-100 bg-[#202020] hover:bg-[#282828] border border-[rgba(255,255,255,0.1)] rounded px-2.5 py-1.5 transition-colors duration-150"
       >
-
         <span>{content}</span>
         {icon}
       </Link>
